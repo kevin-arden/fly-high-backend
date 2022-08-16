5780f2bcc32cbb0517aaa9a32ed87f5ecb09188b
Kevin Arden <kevin.arden@tsmid.com>
2022-08-16 23:27:29 +0700

    Revert "init commit"
    This reverts commit da4ababe1206fa89e6e209ec9a6b63d62f922229.

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000
-const userData = require("./MOCK_DATA.json")
-
-app.listen(PORT, () => {
-  console.log("Running fine")
-})
